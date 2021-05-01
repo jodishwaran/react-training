789c709f36c5167373c52c41054c9846df720dc8
ganesh <jodishwaran@gmail.com>
2021-05-02 00:03:49 +0530

    simplified data

--- src/components/Fruit.jsx
+++ src/components/Fruit.jsx
@@ -2,20 +2,13 @@ import "./Fruit.css";
 import Card from "./Card";
 
 function Fruit(props) {
-  const { name, price, description, img } = props;
+  const { name, price, img } = props;
   console.log("I am Fruit -  rendering");
   return (
     <Card className="fruit">
       <Card className="fruit__content">
-        <div>
-          <b>Name :</b> {name}
-        </div>
-        <div>
-          <b>Price :</b> {price}$
-        </div>
-        <div>
-          <b>Description :</b> {description}
-        </div>
+        <div>{name}</div>
+        <div>{price}$</div>
       </Card>
       <img className="fruit__img" src={img} alt="" />
     </Card>
